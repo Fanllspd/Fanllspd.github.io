aca71e731736d2f31ae9f4ad8f3857e4a413e0d8
Fanllspd <Fanllspd@gmail.com>
2024-02-07 16:11:00 +0800

    Site updated: 2024-02-07 16:10:55

--- js/walinePlaceHolder.js
+++
@@ -1,8 +0,0 @@
-fetch('https://v1.hitokoto.cn')
-  .then((response) => response.json())
-  .then((data) => {
-    document
-      .getElementById('wl-edit')
-      .setAttribute('placeholder', data.hitokoto + '__' + data.from)
-  })
-  .catch(console.error)
